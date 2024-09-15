760024ede3a0f03ff92b9c90a22a6dca84a7b702
Pranil Chavda <pranil@Pranils-MacBook-Air.local>
2024-09-15 18:05:26 +0530

    Finally loading page is done

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
 
 
 const loaderTimer= document.querySelector('#counting h4');
-let grow=1;
-
-setInterval(function() {
-    if(grow<100){
-        loaderTimer.innerHTML = grow++;
-        // console.log(grow);
-    }else{ loaderTimer.innerHTML = 100;}
-}, 35)
-
 
 let TL = gsap.timeline();
-TL.from("#counting" , {
-    x: -100,
-    stagger: 0.2,
-    duration: 0.4,
-    ease: "expo.inOut",
-    autoAlpha: 0, 
-})
-TL.from(".statement h1", {
+TL.from(".statement h2", {
     y: 160,
+    opacity:0,
     stagger: 0.2,
-    duration: 0.7,
+    duration: 0.8,
     ease: "expo.inOut",
     autoAlpha: 0,
     // delay: 0.8,
 })
-// TL.from("#counting" , {
-//     x: -100,
-//     stagger: 0.2,
-//     duration: 0.1,
-//     ease: "expo.inOut",
-//     autoAlpha: 0, 
-// })
+TL.from("#counting,.statement h3" , {
+    // x: -38,
+    stagger: 0.2,
+    opacity:0,
+    duration: 0.4,
+    ease: "expo.inOut",
+    autoAlpha: 0, 
+    onStart : function(){
+        let grow=1;     
+        setInterval(function() {
+            if(grow<100){
+                loaderTimer.innerHTML = grow++;
+                // console.log(grow);
+            }
+            else{ loaderTimer.innerHTML = 100;
+            }
+        }, 25)
+    }, 
+})
+
 TL.to("#loader",{
     opacity:0,
-    duration:0.6,
-    delay:3
+    duration:0.4,
+    delay:2
+})
+TL.from('#page1',{
+    y:1200,
+    opacity:0.5,
+    delay:0.3,
+    ease: "expo.inOut",
+})
+TL.to('#loader',{
+    display: none,
 })
