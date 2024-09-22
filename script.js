f2824221dd5d5cbe209a3c5bbc0f34aab7a3b23d
Pranil Chavda <pranilchavda@gmail.com>
2024-09-22 08:55:40 +0530

    Page3 started

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ ScrollTrigger.refresh();
 
 }
 
-
 function loadingPageAnimation(){
 const loaderTimer= document.querySelector('#counting h4');
 
@@ -106,6 +105,17 @@ function cursorAnimation(){
         // duration: 1,
       });
     }
+
+
+    function imgAnimation(){
+        Shery.imageEffect(".imgdiv", {
+            style:5,
+            // debug:true,
+            //config: {"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7272667488697082},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.43,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.4,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+            gooey: true,
+        })
+    }
+imgAnimation();
 
 //function executions 
 loadingPageAnimation();
